c8b43e09446b2d1a6b10bbecb10fa2e398e8d1d0
Rafael Oliveira <ludug3r0@gmail.com>
2012-08-20 10:29:14 +0530

    clean up detection of tests in build file

---
+++ src/unit-tests/java/FailingTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FailingTest {
+
+    @Test
+    public void failingTest() {
+        assertTrue(false);
+    }
+}
---
+++ src/unit-tests/java/FailingTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FailingTests {
+
+    @Test
+    public void failingTest() {
+        assertTrue(false);
+    }
+}
