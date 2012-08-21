499a56f7a9a9895905cfdc308060f663a534e97a
Yahya <yahyapo@thoughtworks.com>
2012-08-21 16:03:06 +0530

    Yahya and Sharanya - Integration of UserService with the LoginController

--- src/main/java/com/zombiedash/app/controller/LoginController.java
+++ src/main/java/com/zombiedash/app/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.zombiedash.app.controller;
 
 import com.zombiedash.app.model.User;
+import com.zombiedash.app.model.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/login")
 
 public class LoginController {
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = "LoginForm", method = RequestMethod.GET)
     public String showForm() {
@@ -20,9 +24,9 @@ public class LoginController {
 
     @RequestMapping(value = "Authenticate", method = RequestMethod.POST)
     public String processForm(@RequestParam("Username") String username, @RequestParam("Password") String password, HttpServletRequest request) {
-        User givenUser = new User(username, password.toCharArray());
+        User givenUser = userService.createAdmin();
         try{
-            givenUser.authenticate("Yahya", "12".toCharArray());
+            givenUser.authenticate(username, password.toCharArray());
             return "loginsuccess";
         }
         catch (Exception e){
--- src/unit-tests/java/com/zombiedash/app/controller/LoginControllerTest.java
+++ src/unit-tests/java/com/zombiedash/app/controller/LoginControllerTest.java
@@ -3,12 +3,13 @@ package com.zombiedash.app.controller;
 import com.zombiedash.app.controller.LoginController;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
 
 import javax.servlet.http.HttpServletRequest;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
-
+@ContextConfiguration(locations = "classpath:/applicationContext.xml")
 public class LoginControllerTest {
     @Test
     public void shouldReturnLoginFormViewName() {
@@ -20,15 +21,15 @@ public class LoginControllerTest {
     @Test
     public void  shouldReturnLoginSuccessViewNameForBothCorrectInput() {
 
-        String viewName = new LoginController().processForm("Yahya","12",new MockHttpServletRequest());
+        String viewName = new LoginController().processForm("admin","Welcome1",new MockHttpServletRequest());
 
         assertThat(viewName,equalTo("loginsuccess"));
     }
 
     @Test
     public void shouldReturnLoginFormViewNameForAnyIncorrectInput(){
-        String viewNameOne = new LoginController().processForm("Yahya", "123", new MockHttpServletRequest());
-        String viewNameTwo = new LoginController().processForm("Yahaaa", "12", new MockHttpServletRequest());
+        String viewNameOne = new LoginController().processForm("admin", "123", new MockHttpServletRequest());
+        String viewNameTwo = new LoginController().processForm("Yahaaa", "Welcome1", new MockHttpServletRequest());
         String viewNameThree = new LoginController().processForm("Yahyaaa", "123", new MockHttpServletRequest());
 
         assertThat(viewNameOne,equalTo("loginform"));
