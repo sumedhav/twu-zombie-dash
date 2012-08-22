c8d4d87ddbb33a2b904a32854f084b0fd1963bdf
Christopher Schuster <cpschuster@uchicago.edu>
2012-08-22 11:23:09 +0530

    [Chris, Yahya] ConferenceRepository integrates with database in IDE. #Admin Create And Read Conference In The System

--- src/main/java/com/zombiedash/app/repository/ConferenceRepository.java
+++ src/main/java/com/zombiedash/app/repository/ConferenceRepository.java
@@ -3,31 +3,48 @@ package com.zombiedash.app.repository;
 import com.zombiedash.app.model.Conference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public class ConferenceRepository {
-    public static final String SQL_CONFERENCE_INSERT = "INSERT INTO Conference values (?,?,?,?,?,?,?,?,?,?)";
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  public static final String SQL_CONFERENCE_INSERT = "INSERT INTO Conference values (?,?,?,?,?,?,?,?,?,?)";
+  public static final String SQL_CONFERENCE_SELECT = "SELECT * FROM Conference WHERE name = ?";
 
-    @Autowired
-    public ConferenceRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-    public Integer saveConference(Conference conference) {
-        return jdbcTemplate.update(SQL_CONFERENCE_INSERT,
-                conference.getName(),
-                conference.getTopic(),
-                conference.getDescription(),
-                conference.getVenue(),
-                conference.getStartDate(),
-                conference.getEndDate(),
-                conference.getMaxAttendee(),
-                conference.getOrganiserName(),
-                conference.getOrganiserContactNumber(),
-                conference.getOrganiserEmail());
-    }
+  @Autowired
+  public ConferenceRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public Integer saveConference(Conference conference) {
+    return jdbcTemplate.update(SQL_CONFERENCE_INSERT,
+        conference.getName(),
+        conference.getTopic(),
+        conference.getDescription(),
+        conference.getVenue(),
+        conference.getStartDate(),
+        conference.getEndDate(),
+        conference.getMaxAttendee(),
+        conference.getOrganiserName(),
+        conference.getOrganiserContactNumber(),
+        conference.getOrganiserEmail());
+  }
+
+  public Conference showConference(String conferenceName) {
+    SqlRowSet rowSet = jdbcTemplate.queryForRowSet(SQL_CONFERENCE_SELECT,conferenceName);
+    rowSet.first();
+    return new Conference(rowSet.getString(1),
+        rowSet.getString(2),
+        rowSet.getString(3),
+        rowSet.getString(4),
+        rowSet.getString(5),
+        rowSet.getString(6),
+        rowSet.getInt(7),
+        rowSet.getString(8),
+        rowSet.getString(9),
+        rowSet.getString(10));
+  }
 }
--- src/unit-tests/java/com/zombiedash/app/repository/ConferenceRepositoryTest.java
+++ src/unit-tests/java/com/zombiedash/app/repository/ConferenceRepositoryTest.java
@@ -2,32 +2,56 @@ package com.zombiedash.app.repository;
 
 import com.zombiedash.app.model.Conference;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ConferenceRepositoryTest {
-    @Test
-    public void shouldNotThrowErrorAndSaveConferenceToDatabase() throws Exception {
-        JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
-        when(jdbcTemplate.update(ConferenceRepository.SQL_CONFERENCE_INSERT, "","","","","","",1,"","","")).thenReturn(1);
-        ConferenceRepository conferenceRepository = new ConferenceRepository(jdbcTemplate);
-        Conference conference = mock(Conference.class);
-        when(conference.getDescription()).thenReturn("");
-        when(conference.getEndDate()).thenReturn("");
-        when(conference.getMaxAttendee()).thenReturn(1);
-        when(conference.getName()).thenReturn("");
-        when(conference.getOrganiserContactNumber()).thenReturn("");
-        when(conference.getOrganiserEmail()).thenReturn("");
-        when(conference.getOrganiserName()).thenReturn("");
-        when(conference.getStartDate()).thenReturn("");
-        when(conference.getTopic()).thenReturn("");
-        when(conference.getVenue()).thenReturn("");
-        int numberOfRows = conferenceRepository.saveConference(conference);
-        assertThat(numberOfRows,is(equalTo(1)));
-    }
+  @Test
+  public void shouldNotThrowErrorAndSaveConferenceToDatabase() throws Exception {
+    JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+    when(jdbcTemplate.update(ConferenceRepository.SQL_CONFERENCE_INSERT, "","","","","","",0,"","","")).thenReturn(1);
+    ConferenceRepository conferenceRepository = new ConferenceRepository(jdbcTemplate);
+    Conference conference = mock(Conference.class);
+    when(conference.getDescription()).thenReturn("");
+    when(conference.getEndDate()).thenReturn("");
+    when(conference.getMaxAttendee()).thenReturn(0);
+    when(conference.getName()).thenReturn("");
+    when(conference.getOrganiserContactNumber()).thenReturn("");
+    when(conference.getOrganiserEmail()).thenReturn("");
+    when(conference.getOrganiserName()).thenReturn("");
+    when(conference.getStartDate()).thenReturn("");
+    when(conference.getTopic()).thenReturn("");
+    when(conference.getVenue()).thenReturn("");
+    int numberOfRows = conferenceRepository.saveConference(conference);
+    assertThat(numberOfRows,is(equalTo(1)));
+  }
+
+  @Test
+  public void shouldReadConferenceFromDatabase() {
+
+    JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+    when(jdbcTemplate.queryForRowSet(ConferenceRepository.SQL_CONFERENCE_SELECT,"java"))
+        .thenAnswer(new Answer<SqlRowSet>() {
+          @Override
+          public SqlRowSet answer(InvocationOnMock invocation) throws Throwable {
+            SqlRowSet sqlRowSet = mock(SqlRowSet.class);
+            when(sqlRowSet.getString(anyInt())).thenReturn("java");
+            when(sqlRowSet.getString(6)).thenReturn("2012-06-07");
+            when(sqlRowSet.getInt(anyInt())).thenReturn(0);
+            return sqlRowSet;
+          }
+        });
+    ConferenceRepository conferenceRepository = new ConferenceRepository(jdbcTemplate);
+    Conference actualConference = conferenceRepository.showConference("java");
+    assertThat(actualConference.getEndDate(),is(equalTo("2012-06-07")));
+  }
 }
