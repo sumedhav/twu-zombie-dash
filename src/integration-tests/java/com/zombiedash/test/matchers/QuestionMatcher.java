e1eda448f5c5e3c8cecf729de66987e62e556bdb
RahulKavale <kavale.rahul@gmail.com>
2012-08-22 12:18:31 +0530

    Venki, Charles - implemented the matches method to include question options

--- src/integration-tests/java/com/zombiedash/test/matchers/QuestionMatcher.java
+++ src/integration-tests/java/com/zombiedash/test/matchers/QuestionMatcher.java
@@ -1,21 +1,44 @@
 package com.zombiedash.test.matchers;
 
+import com.zombiedash.app.model.Option;
 import com.zombiedash.app.model.Question;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 
+import java.util.List;
 import java.util.Map;
 
 public class QuestionMatcher extends BaseMatcher<Question> {
     private final String text;
+    private Map<String,Boolean> options;
 
     private QuestionMatcher(String text, Map<String, Boolean> options) {
         this.text = text;
+        this.options = options;
     }
 
     @Override
     public boolean matches(Object item) {
         Question question = (Question) item;
+        return validateQuestionText(question) && validateQuestionOptions(question);
+    }
+
+    private boolean validateQuestionOptions(Question question) {
+        List<Option> questionOptions = question.getOptions();
+
+        if(options.size() != questionOptions.size())
+            return false;
+
+        for(Option option : questionOptions) {
+            if(!(options.containsKey(option.getText())
+                    && options.get(option.getText()).equals(option.isCorrect())))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateQuestionText(Question question) {
         return question.getText().equals(text);
     }
 
@@ -27,4 +50,6 @@ public class QuestionMatcher extends BaseMatcher<Question> {
     public static QuestionMatcher aQuestionWith(String text, Map<String, Boolean> options) {
         return new QuestionMatcher(text, options);
     }
+
+
 }
