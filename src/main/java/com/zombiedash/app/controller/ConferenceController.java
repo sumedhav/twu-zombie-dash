0b1df6fb5809d30f33b5f89353adb8e8af24de0b
Christopher Schuster <cpschuster@uchicago.edu>
2012-08-24 14:22:31 +0530

    [Charles, Chris] Dev work done  #Admin Creates And Reads Conference in the System

--- src/main/java/com/zombiedash/app/controller/ConferenceController.java
+++ src/main/java/com/zombiedash/app/controller/ConferenceController.java
@@ -2,6 +2,7 @@ package com.zombiedash.app.controller;
 
 import com.zombiedash.app.model.Conference;
 import com.zombiedash.app.repository.ConferenceRepository;
+import com.zombiedash.app.validator.ConferenceValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,63 +12,78 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
 @RequestMapping("/admin/conference")
 public class ConferenceController {
-    private ConferenceRepository conferenceRepository;
+  private ConferenceRepository conferenceRepository;
 
-    @Autowired
-    public ConferenceController(ConferenceRepository conferenceRepository) {
-        this.conferenceRepository = conferenceRepository;
+  @Autowired
+  public ConferenceController(ConferenceRepository conferenceRepository) {
+    this.conferenceRepository = conferenceRepository;
+  }
+
+  @RequestMapping(value = "createConference", method = RequestMethod.GET)
+  public ModelAndView createConference() {
+    return new ModelAndView("createconference");
+  }
+
+  @RequestMapping(value = "home",method = RequestMethod.GET)
+  public ModelAndView home() {
+    List<Conference> conferenceList = conferenceRepository.showAllConferences();
+    List<String> conferenceNames = new ArrayList<String>();
+    for (Conference conference : conferenceList) {
+      conferenceNames.add(conference.getName());
+    }
+    return new ModelAndView("conferencehome","Conferences", conferenceNames);
+  }
+
+  @RequestMapping(value = "submit", method = RequestMethod.POST)
+  public ModelAndView submit(@RequestParam("conf_name") String conferenceName,
+                             @RequestParam("conf_topic") String conferenceTopic,
+                             @RequestParam("conf_description") String conferenceDescription,
+                             @RequestParam("conf_venue") String conferenceVenue,
+                             @RequestParam("conf_start_date") String conferenceStartDate,
+                             @RequestParam("conf_end_date") String conferenceEndDate,
+                             @RequestParam("conf_max_attendees") String conferenceMaxAttendees) {
+
+    Map<String,String> model = new HashMap<String, String>();
+    model.put("name",conferenceName);
+    model.put("topic",conferenceTopic);
+    model.put("startDate",conferenceStartDate);
+    model.put("endDate",conferenceEndDate);
+    model.put("description",conferenceDescription);
+    model.put("venue",conferenceVenue);
+    model.put("maxAttendees", conferenceMaxAttendees);
+
+    ConferenceValidator conferenceValidator = new ConferenceValidator();
+    boolean validDataFlag = conferenceValidator.isValidNumber(conferenceMaxAttendees, model);
+    validDataFlag &= conferenceValidator.isNotEmpty(conferenceName, conferenceTopic, conferenceDescription,
+        conferenceVenue, conferenceStartDate, conferenceEndDate, conferenceMaxAttendees, model);
+    validDataFlag &= conferenceValidator.isValidDate(conferenceStartDate, conferenceEndDate, model);
+
+    if (validDataFlag) {
+      Conference conference = new Conference(conferenceName, conferenceTopic, conferenceDescription,
+          conferenceVenue, conferenceStartDate, conferenceEndDate, Integer.parseInt(conferenceMaxAttendees));
+      conferenceRepository.saveConference(conference);
+      return new ModelAndView("conferencehome");
+    } else {
+      return new ModelAndView("createconference", "model", model);
     }
 
-    @RequestMapping(value = "createConference", method = RequestMethod.GET)
-    public ModelAndView createConference() {
-        return new ModelAndView("createconference");
-    }
 
-    @RequestMapping(value = "home",method = RequestMethod.GET)
-    public ModelAndView home() {
-        List<Conference> conferenceList = conferenceRepository.showAllConferences();
-        List<String> conferenceNames = new ArrayList<String>();
-        for (Conference conference : conferenceList) {
-            conferenceNames.add(conference.getName());
-        }
-        return new ModelAndView("conferencehome","Conferences", conferenceNames);
-    }
+  }
 
-    @RequestMapping(value = "submit", method = RequestMethod.POST)
-    public ModelAndView submit(@RequestParam("conf_name") String conferenceName,
-                               @RequestParam("conf_topic") String conferenceTopic,
-                               @RequestParam("conf_start_date") String conferenceStartDate,
-                               @RequestParam("conf_end_date") String conferenceEndDate,
-                               @RequestParam("conf_description") String conferenceDescription,
-                               @RequestParam("conf_venue") String conferenceVenue,
-                               @RequestParam("conf_max_attendees") String conferenceMaxAttendees) {
-        Conference conference;
-        try {
-            conference = new Conference(
-                    conferenceName,
-                    conferenceTopic,
-                    conferenceDescription,
-                    conferenceVenue,
-                    conferenceStartDate,
-                    conferenceEndDate,
-                    Integer.parseInt(conferenceMaxAttendees));
-        } catch (RuntimeException e) {
-            return new ModelAndView("createconference","ErrorString", "ALL FIELDS ARE COMPULSORY");
-        }
-        conferenceRepository.saveConference(conference);
-        return new ModelAndView("conferencehome");
-    }
 
-    @RequestMapping(value = "view/{conferenceName}")
-    public ModelAndView view(@PathVariable String conferenceName) {
-        Conference thisConference = conferenceRepository.showConference(conferenceName);
-        return new ModelAndView("conferenceview","Conference", thisConference);
-    }
+
+  @RequestMapping(value = "view/{conferenceName}")
+  public ModelAndView view(@PathVariable String conferenceName) {
+    Conference thisConference = conferenceRepository.showConference(conferenceName);
+    return new ModelAndView("conferenceview","Conference", thisConference);
+  }
 
 }
---
+++ src/main/java/com/zombiedash/app/validator/ConferenceValidator.java
@@ -0,0 +1,46 @@
+package com.zombiedash.app.validator;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+public class ConferenceValidator {
+  public boolean isValidNumber(String conferenceMaxAttendees, Map<String, String> model) {
+    try {
+      Integer theInt = Integer.parseInt(conferenceMaxAttendees);
+      if (theInt > 0) return true;
+    } catch (NumberFormatException e) {
+    }
+    model.put("numberError", "invalid data");
+    return false;
+  }
+
+  public boolean isNotEmpty(String name, String topic, String description,
+                            String venue, String startDate, String endDate,
+                            String maxAttendee, Map<String, String> model) {
+    if (name.isEmpty() ||
+        topic.isEmpty() ||
+        description.isEmpty() ||
+        venue.isEmpty() ||
+        startDate.isEmpty() ||
+        endDate.isEmpty() ||
+        maxAttendee.isEmpty()) {
+      model.put("errorString", "ALL FIELDS ARE COMPULSORY");
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isValidDate(String startDate, String endDate, Map<String, String> model) {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    dateFormat.setLenient(false);
+    try {
+      return (null != dateFormat.parse(startDate)) &&
+          (null != dateFormat.parse(endDate));
+    } catch (ParseException e) {
+      model.put("dateError", "Date must be in yyyy-MM-dd format");
+      return false;
+    }
+  }
+}
