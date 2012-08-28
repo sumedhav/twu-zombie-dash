42909748eca72c20c23b623780a4ca81e66172f4
Yahya <yahyapo@thoughtworks.com>
2012-08-28 15:55:22 +0530

    Yahya and Rahul - Decoupled test data for manual testing from database creation and changed QuestionRepository to handle boolean in PostGre also

--- src/integration-tests/java/com/zombiedash/app/repository/QuestionRepositoryTest.java
+++ src/integration-tests/java/com/zombiedash/app/repository/QuestionRepositoryTest.java
@@ -9,10 +9,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import static com.zombiedash.app.repository.QuestionMatcher.aQuestionWith;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertThat;
@@ -24,12 +26,23 @@ public class QuestionRepositoryTest extends AbstractTransactionalJUnit4SpringCon
     private JdbcTemplate jdbcTemplate;
     private List<Option> anOptionList;
 
+    private void setUpQuestions(){
+        givenAQuestionWith(10, "Where is Red Fort");
+        givenAnOptionFor(10, 10, "Delhi", true);
+        givenAnOptionFor(10, 20, "Paris", false);
+        givenAnOptionFor(10, 30, "New York", false);
+        givenAQuestionWith(20, "Is it lunch time?");
+        givenAnOptionFor(20, 10, "I bet it is", true);
+        givenAnOptionFor(20, 20, "No thanks, fasting at the moment", false);
 
+    }
     @Test
     public void shouldRetrieveAllQuestions() {
+        int alreadyAvailableQuestions=jdbcTemplate.queryForInt("SELECT count(*) FROM zombie_question");
+        setUpQuestions();
         QuestionRepository questionRepository = new QuestionRepository(jdbcTemplate);
         List<Question> questions = questionRepository.listAllQuestions();
-        assertThat(questions.size(), is(2));
+        assertThat(questions.size(), is(alreadyAvailableQuestions+2));
         Matcher<Iterable<Question>> matchTheInsertedQuestions = hasItems(
                 QuestionMatcher.aQuestionWith("Where is Red Fort",
                         new HashMap<String, Boolean>() {{
@@ -45,5 +58,29 @@ public class QuestionRepositoryTest extends AbstractTransactionalJUnit4SpringCon
         );
         assertThat(questions, matchTheInsertedQuestions);
     }
+
+    @Test
+    public void shouldReturnCorrectOption(){
+        anOptionList = new ArrayList<Option>();
+        anOptionList.add(new Option(1, "Bangalore", true));
+        anOptionList.add(new Option(2, "Paris", false));
+        anOptionList.add(new Option(3, "Johannesburg", false));
+
+        String expectedCorrectOption = "Bangalore";
+        Question aQuestion = new Question(3, "Where are you?", anOptionList);
+
+        assertThat(aQuestion.getValidOption(), equalTo(expectedCorrectOption));
+    }
+    private void givenAQuestionWith(int id, String text) {
+        jdbcTemplate.execute(String.format(
+                "insert into zombie_Question (ID,Text) values(%d, '%s')", id, text));
+    }
+
+    private void givenAnOptionFor(int questionId, int optionId, String text, boolean correct) {
+        jdbcTemplate.execute(String.format("insert into zombie_Option (id,question_id,text,correct) " +
+                "values (%d, %d, '%s', %b)",
+                optionId, questionId, text, correct));
+    }
+
 }
 
