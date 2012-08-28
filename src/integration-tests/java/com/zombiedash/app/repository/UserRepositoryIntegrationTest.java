3782771cd521f7a70e0e151500407be548fa1170
Rafael Oliveira <ludug3r0@gmail.com>
2012-08-28 18:36:04 +0530

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 12cbd321db0c3726cde46d5d777b0f760de0bcb4, reversing
    changes made to e08d6779326f25aea9b83748affee165762f8bf5.

--- src/integration-tests/java/com/zombiedash/app/repository/UserRepositoryIntegrationTest.java
+++ src/integration-tests/java/com/zombiedash/app/repository/UserRepositoryIntegrationTest.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static com.zombiedash.app.test.matchers.UserMatcher.isAUserWith;
-import static org.apache.commons.codec.digest.DigestUtils.sha512Hex;
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -68,12 +68,12 @@ public class UserRepositoryIntegrationTest {
     public void shouldHashUserPassword() throws Exception {
         userRepository.createUser(new User("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"), "password1");
         String result = jdbcTemplate.queryForObject("SELECT password FROM zombie_users WHERE username = 'designer'", String.class);
-        assertThat(result , is(sha512Hex("password1")));
+        assertThat(result , is(md5Hex("password1")));
     }
 
     @Test
     public void shouldLookUpAuthenticationCredentialsAndReturnUsernameIfFound() throws Exception {
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', '" + sha512Hex("password1") + "', 1, 'Game Designer 1', 'gm1@zombie.com')");
+        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', '" + md5Hex("password1") + "', 1, 'Game Designer 1', 'gm1@zombie.com')");
         User actualUser = userRepository.getUser("beta123", "password1");
 
         assertThat(actualUser, isAUserWith("beta123", Role.GAME_DESIGNER, "Game Designer 1", "gm1@zombie.com"));
