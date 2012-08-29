d0e1fbaee0f9db704f166a3a2408b03c2bf048ee
Christopher Schuster <cpschuster@uchicago.edu>
2012-08-29 15:03:28 +0530

    [Chris, Rafael] QA scrutiny on UserRepository #QA Create Admin User

--- src/integration-tests/java/com/zombiedash/app/repository/UserRepositoryIntegrationTest.java
+++ src/integration-tests/java/com/zombiedash/app/repository/UserRepositoryIntegrationTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import static com.zombiedash.app.test.matchers.UserMatcher.isAUserWith;
 import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -24,79 +25,101 @@ import static org.junit.Assert.assertThat;
 @ContextConfiguration(locations = "/test-application-context.xml")
 public class UserRepositoryIntegrationTest {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-    @Before
-    public void setUp() throws Exception {
-        jdbcTemplate.execute("DELETE zombie_users");
-        userRepository = new UserRepository(jdbcTemplate);
-    }
+  @Before
+  public void setUp() throws Exception {
+    jdbcTemplate.execute("DELETE zombie_users");
+    userRepository = new UserRepository(jdbcTemplate);
+  }
 
-    @Test
-    public void shouldRetrieveUser() {
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('admin', 'Welcome1', 0, 'Administrator', 'admin@zombie.com')");
-        User result = userRepository.getUser("admin");
-        assertThat(result, isAUserWith("admin", Role.ADMIN, "Administrator", "admin@zombie.com"));
-    }
+  @Test
+  public void shouldRetrieveAllUsers() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('admin', 'Welcome1', 0, 'Administrator', 'admin@zombie.com')");
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', 'password1', 1, 'First Game Designer', 'gm1@zombie.com')");
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('charlie', 'qwerty1234', 1, 'Second Game Designer', 'gm2@zombie.com')");
+    List<User> result = userRepository.retrieveAllUsers();
+    assertThat(result.get(0), isAUserWith("admin", Role.ADMIN, "Administrator", "admin@zombie.com"));
+    assertThat(result.get(1), isAUserWith("beta123", Role.GAME_DESIGNER, "First Game Designer", "gm1@zombie.com"));
+    assertThat(result.get(2), isAUserWith("charlie", Role.GAME_DESIGNER, "Second Game Designer", "gm2@zombie.com"));
 
-    @Test(expected = RuntimeException.class)
-    public void shouldThrowRuntimeExceptionIfNoUserIsFound() {
-        User result = userRepository.getUser("whatever");
-        assertThat(result, isAUserWith("admin", Role.ADMIN, "Administrator", "admin@zombie.com"));
-    }
+  }
 
-    @Test
-    public void shouldRetrieveAllUsers() {
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('admin', 'Welcome1', 0, 'Administrator', 'admin@zombie.com')");
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', 'password1', 1, 'Game Designer 1', 'gm1@zombie.com')");
-        List<User> result = userRepository.retrieveAllUsers();
-        assertThat(result.get(0), isAUserWith("admin", Role.ADMIN, "Administrator", "admin@zombie.com"));
-        assertThat(result.get(1), isAUserWith("beta123", Role.GAME_DESIGNER, "Game Designer 1", "gm1@zombie.com"));
-    }
+  @Test
+  public void shouldRetrieveGivenUserIfUserExists() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('admin', 'Welcome1', 0, 'Administrator', 'admin@zombie.com')");
+    User result = userRepository.getUser("admin");
+    assertThat(result, isAUserWith("admin", Role.ADMIN, "Administrator", "admin@zombie.com"));
+  }
 
-    @Test
-    public void shouldCreateUser() {
-        userRepository.createUser(new User("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"), "password1");
-        User actualUser = retrieveUserByUsername("designer");
-        assertThat(actualUser, isAUserWith("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"));
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldThrowRuntimeExceptionIfNoUserIsFound() {
+    userRepository.getUser("");
+  }
 
-    @Test
-    public void shouldHashUserPassword() throws Exception {
-        userRepository.createUser(new User("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"), "password1");
-        String result = jdbcTemplate.queryForObject("SELECT password FROM zombie_users WHERE username = 'designer'", String.class);
-        assertThat(result , is(md5Hex("password1")));
-    }
+  @Test
+  public void shouldLookUpAuthenticationCredentialsAndReturnUsernameIfFound() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', '" + md5Hex("password1") + "', 1, 'Game Designer 1', 'gm1@zombie.com')");
+    User actualUser = userRepository.getUser("beta123", "password1");
+    assertThat(actualUser, isAUserWith("beta123", Role.GAME_DESIGNER, "Game Designer 1", "gm1@zombie.com"));
+  }
 
-    @Test
-    public void shouldLookUpAuthenticationCredentialsAndReturnUsernameIfFound() throws Exception {
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta123', '" + md5Hex("password1") + "', 1, 'Game Designer 1', 'gm1@zombie.com')");
-        User actualUser = userRepository.getUser("beta123", "password1");
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldThrowInvalidArgumentExceptionIfCredentialsAreBad() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('username', '" + md5Hex("password") + "', 1, '', '')");
+    userRepository.getUser("beta123", "password1");
+  }
 
-        assertThat(actualUser, isAUserWith("beta123", Role.GAME_DESIGNER, "Game Designer 1", "gm1@zombie.com"));
-    }
+  @Test
+  public void shouldCreateUser() {
+    Boolean userCreationFlag = userRepository.createUser(new User("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"), "password1");
+    User actualUser = retrieveUserByUsername("designer");
+    assertThat(userCreationFlag,is(equalTo(true)));
+    assertThat(actualUser, isAUserWith("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"));
+  }
 
-    @Test
-    public void shouldDeleteUserFromDatabase(){
-        jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta', 'password1', 1, 'Game Designer 1', 'gm1@zombie.com')");
-        userRepository.deleteUser("beta");
-        assertThat(jdbcTemplate.queryForInt("SELECT count(*) FROM zombie_users WHERE username = 'beta'"), is(0));
-    }
+  @Test
+  public void shouldHashUserPassword() throws Exception {
+    userRepository.createUser(new User("designer", Role.GAME_DESIGNER, "MR.Right", "right@rightmail.com"), "password1");
+    String result = jdbcTemplate.queryForObject("SELECT password FROM zombie_users WHERE username = 'designer'", String.class);
+    assertThat(result , is(equalTo(md5Hex("password1"))));
+  }
 
-    private User retrieveUserByUsername(String username) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM zombie_users WHERE username = '" + username + "'", new RowMapper() {
-            @Override
-            public Object mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new User(resultSet.getString("username"),
-                        Role.generateRole(resultSet.getString("role")),
-                        resultSet.getString("name"),
-                        resultSet.getString("email"));
-            }
-        });
-        assertThat(users.size(), is(1));
-        return users.get(0);
-    }
+  @Test
+  public void shouldDeleteUserFromDatabase() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('beta', 'password1', 1, 'Game Designer 1', 'gm1@zombie.com')");
+    userRepository.deleteUser("beta");
+    assertThat(jdbcTemplate.queryForInt("SELECT count(*) FROM zombie_users WHERE username = 'beta'"), is(0));
+  }
+
+  @Test
+  public void existentUsernameShouldReturnTrue() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('username', '', 0, '', '')");
+    Boolean usernameExistsFlag = userRepository.userNameExists("username");
+    assertThat(usernameExistsFlag, is(equalTo(true)));
+  }
+
+  @Test
+  public void nonexistentUsernameShouldReturnFalse() {
+    jdbcTemplate.execute("INSERT INTO zombie_users VALUES('username', '', 0, '', '')");
+    Boolean usernameExistsFlag = userRepository.userNameExists("");
+    assertThat(usernameExistsFlag, is(equalTo(false)));
+  }
+
+  private User retrieveUserByUsername(String username) {
+    List<User> users = jdbcTemplate.query("SELECT * FROM zombie_users WHERE username = '" + username + "'", new RowMapper<User>() {
+      @Override
+      public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        return new User(resultSet.getString("username"),
+            Role.generateRole(resultSet.getString("role")),
+            resultSet.getString("name"),
+            resultSet.getString("email"));
+      }
+    });
+    assertThat(users.size(), is(1));
+    return users.get(0);
+  }
 }
--- src/main/java/com/zombiedash/app/repository/UserRepository.java
+++ src/main/java/com/zombiedash/app/repository/UserRepository.java
@@ -15,72 +15,71 @@ import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
 @Repository
 public class UserRepository {
-    private JdbcTemplate jdbcTemplate;
-    private static final String RETRIEVE_USER_ROW = "SELECT * FROM zombie_users where role = ?";
-    private static final String RETRIEVE_USER_BY_USERNAME = "SELECT * FROM zombie_users where username = ?";
-    private static final String RETRIEVE_USER_BY_USERNAME_AND_PASSWORD = "SELECT * FROM zombie_users where username = ? and password = ?";
-    private static final String RETRIEVE_ALL_USERS = "SELECT * FROM zombie_users";
-    private static final String INSERT_USER = "INSERT INTO zombie_users values (?,?,?,?,?)";
+  private JdbcTemplate jdbcTemplate;
+  private static final String RETRIEVE_USER_BY_USERNAME = "SELECT * FROM zombie_users where username = ?";
+  private static final String RETRIEVE_USER_BY_USERNAME_AND_PASSWORD = "SELECT * FROM zombie_users where username = ? and password = ?";
+  private static final String RETRIEVE_ALL_USERS = "SELECT * FROM zombie_users";
+  private static final String INSERT_USER = "INSERT INTO zombie_users values (?,?,?,?,?)";
 
-    @Autowired
-    public UserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+  @Autowired
+  public UserRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
-    public List<User> retrieveAllUsers() {
-        return jdbcTemplate.query(RETRIEVE_ALL_USERS, userMapper());
-    }
+  public List<User> retrieveAllUsers() {
+    return jdbcTemplate.query(RETRIEVE_ALL_USERS, userMapper());
+  }
 
-    public User getUser(String username){
-        List<User> userList = retrieveAllUsersWithUserName(username);
-        if(userList.size() == 0) throw new RuntimeException("Username not found");
-        return userList.get(0);
-    }
+  public User getUser(String username){
+    List<User> userList = retrieveAllUsersWithUserName(username);
+    if(userList.size() == 0) throw new IllegalArgumentException("Username not found");
+    return userList.get(0);
+  }
 
-    public User getUser(String username, String password) throws Exception {
-        Object[] arg = new Object[]{username, md5Hex(password)};
-        List<User> userList = jdbcTemplate.query(RETRIEVE_USER_BY_USERNAME_AND_PASSWORD, arg, userMapper());
-        if(userList.size() == 0) throw new Exception("Invalid user credentials");
-        return userList.get(0);
-    }
+  public User getUser(String username, String password){
+    Object[] arg = new Object[]{username, md5Hex(password)};
+    List<User> userList = jdbcTemplate.query(RETRIEVE_USER_BY_USERNAME_AND_PASSWORD, arg, userMapper());
+    if(userList.size() == 0) throw new IllegalArgumentException("Invalid user credentials");
+    return userList.get(0);
+  }
 
-    private List<User> retrieveAllUsersWithUserName(String username) {
-        Object[] arg = new Object[]{username};
-        return jdbcTemplate.query(RETRIEVE_USER_BY_USERNAME, arg, userMapper());
-    }
+  public Boolean createUser(User user, String password){
+    Integer result = jdbcTemplate.update(INSERT_USER,
+        user.getUserName(),
+        md5Hex(password),
+        user.getRoleVal(),
+        user.getName(),
+        user.getEmail()
+    );
+    return result == 1;
 
-    public Boolean createUser(User user, String password){
-        Integer result = jdbcTemplate.update(INSERT_USER,
-                user.getUserName(),
-                md5Hex(password),
-                user.getRoleVal(),
-                user.getName(),
-                user.getEmail()
-                );
-        return result == 1;
+  }
 
-    }
+  public void deleteUser(String username){
+    if(username.equals("admin")) throw new RuntimeException("Cannot Delete Admin user.");
+    jdbcTemplate.execute("DELETE FROM zombie_users WHERE username = '" + username + "'");
+  }
 
-    public void deleteUser(String username){
-        if(username.equals("admin")) throw new RuntimeException("Cannot Delete Admin user.");
-        jdbcTemplate.execute("DELETE FROM zombie_users WHERE username = '" + username + "'");
-    }
 
-    private RowMapper userMapper() {
-        return new RowMapper() {
-            @Override
-            public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+  public boolean userNameExists(String username) {
+    List<User> users = retrieveAllUsersWithUserName(username);
+    return users.size() != 0;
+  }
 
-                return new User(resultSet.getString("username"),
-                        Role.generateRole(resultSet.getString("role")),
-                        resultSet.getString("name"),
-                        resultSet.getString("email"));
-            }
-        };
-    }
+  private RowMapper<User> userMapper() {
+    return new RowMapper<User>() {
+      @Override
+      public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        return new User(resultSet.getString("username"),
+            Role.generateRole(resultSet.getString("role")),
+            resultSet.getString("name"),
+            resultSet.getString("email"));
+      }
+    };
+  }
 
-    public boolean userNameExists(String username) {
-        List<User> users = retrieveAllUsersWithUserName(username);
-        return users.size()!=0;
-    }
+  private List<User> retrieveAllUsersWithUserName(String username) {
+    Object[] arg = new Object[]{username};
+    return jdbcTemplate.query(RETRIEVE_USER_BY_USERNAME, arg, userMapper());
+  }
 }
