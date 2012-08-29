b27f1b47e53b50768108bfd12a824763cfffd70a
Sumedha <sumedhaverma6@gmail.com>
2012-08-29 11:08:22 +0530

    [Sumedha, Yahya] Added validation unit tests for create user form #Admin CRUD Game designer

--- src/unit-tests/java/com/zombiedash/app/forms/UserFormTest.java
+++ src/unit-tests/java/com/zombiedash/app/forms/UserFormTest.java
@@ -1,6 +1,7 @@
 package com.zombiedash.app.forms;
 
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.springframework.ui.ModelMap;
 
 import java.util.List;
@@ -20,17 +21,55 @@ public class UserFormTest {
     }
 
     @Test
-    public void shouldReturnInvalidPasswordErrorForInvalidPassword() throws Exception {
-        UserForm userForm = new UserForm("username","role","name","email@email.com", "pass");
+    public void shouldReturnInvalidUserNameWhenUserNameHasWhitespaces() {
+        UserForm userForm = new UserForm("user 123","role","name","email@email.com", "password134234fsd");
+        List<String> result = userForm.validate();
+        assertThat(result.get(0),is(equalTo("invalidUserName")));
+    }
+
+    @Test
+    public void shouldReturnInvalidUserNameErrorWhenUserNameIsBelowLowerLimit() throws Exception {
+        UserForm userForm = new UserForm("user","role","name","email@email.com", "password134234fsd");
+        List<String> result = userForm.validate();
+        assertThat(result.get(0),is(equalTo("invalidUserName")));
+    }
+
+    @Test
+    public void shouldReturnInvalidUserNameErrorWhenUserNameIsAboveUpperLimit() throws Exception {
+        UserForm userForm = new UserForm("qwertyuioplkjhgfdsazxcvbnmqwertyuioplkjhg","role","name","email@email.com", "password134234fsd");
+        List<String> result = userForm.validate();
+        assertThat(result.get(0),is(equalTo("invalidUserName")));
+    }
+
+    @Test
+    public void shouldReturnInvalidUserNameErrorWhenUserNameHasSpecialCharacters() throws Exception {
+        UserForm userForm = new UserForm("qwertyuioplkjhgfdsazxcvbnmqwertyu@#","role","name","email@email.com", "password134234fsd");
+        List<String> result = userForm.validate();
+        assertThat(result.get(0),is(equalTo("invalidUserName")));
+
+    }
+
+    @Test
+    public void shouldReturnInvalidPasswordErrorWhenPasswordIsBelowLowerLimit() throws Exception {
+        UserForm userForm = new UserForm("username","role","name","email@email.com", "passw");
+        List<String> result = userForm.validate();
+        assertThat(result.get(0),is(equalTo("invalidPassword")));
+    }
+
+
+    @Test
+    public void shouldReturnInvalidPasswordErrorWhenPasswordIsAboveUpperLimit() throws Exception {
+        UserForm userForm = new UserForm("username","role","name","email@email.com", "qwertyuioplkjhgfdsazxcvbnmqwertyuioplkjhg");
         List<String> result = userForm.validate();
         assertThat(result.get(0),is(equalTo("invalidPassword")));
     }
 
     @Test
-    public void shouldReturnInvalidUserNameErrorForInvalidUserName() throws Exception {
-        UserForm userForm = new UserForm("user","role","name","email@email.com", "password134234fsd");
+    public void shouldReturnInvalidPasswordErrorWhenPasswordHasSpecialCharacters() throws Exception {
+        UserForm userForm = new UserForm("username","role","name","email@email.com", "password134234*&^fsd");
         List<String> result = userForm.validate();
-        assertThat(result.get(0),is(equalTo("invalidUserName")));
+        assertThat(result.get(0),is(equalTo("invalidPassword")));
+
     }
 
     @Test
@@ -38,6 +77,13 @@ public class UserFormTest {
         UserForm userForm = new UserForm("username","role","name","emailemail.com", "password134234fsd");
         List<String> result = userForm.validate();
         assertThat(result.get(0),is(equalTo("invalidEmail")));
+    }
+
+    @Test
+    public void shouldReturnNoErrorsWhenAllCredentialsAreValid() {
+        UserForm userForm = new UserForm("user123","role","name","email@email.com", "password134234fsd");
+        userForm.validate();
+        assertThat(userForm.hasErrors(),is(false));
     }
 
 }
