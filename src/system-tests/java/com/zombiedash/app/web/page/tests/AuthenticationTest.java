d22f771b417174ba9bf9a246f497c38179423894
venkateswaris <venkateswarisrinivasan@gmail.com>
2012-08-30 16:29:37 +0530

    venky, silvio - added logout success url, renamed login test

--- src/system-tests/java/com/zombiedash/app/web/page/tests/AuthenticationTest.java
+++ src/system-tests/java/com/zombiedash/app/web/page/tests/AuthenticationTest.java
@@ -1,12 +1,8 @@
 package com.zombiedash.app.web.page.tests;
 
-import com.zombiedash.app.web.Application;
-import com.zombiedash.app.web.Browser;
 import com.zombiedash.app.web.page.tests.helper.BrowserSessionBuilder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -21,29 +17,23 @@ public class AuthenticationTest extends BasePageTest {
                 .build();
 
         assertThat(browser.getPageTitle(), is("Zombie Dash : Welcome"));
+    }
 
-        //todo: move this into ANOTHER test
-//        statelessBrowser.findElement(By.id("Logout")).click();
-//
-//        assertThat(statelessBrowser.getPageTitle(), is("Zombie Dash : Login"));
-//
+    @Test
+    public void shouldGoToLoginPageAfterLogout() {
+        browser = BrowserSessionBuilder.newStatelessSession().loggedInAsAdmin().build();
+        browser.findElement(By.id("Logout")).click();
+        assertThat(browser.getPageTitle(), is("Zombie Dash : Login"));
+    //todo: do we still need a message?
 //        assertThat(statelessBrowser.findElement(By.id("message_to_be_displayed")).getText(), is("You have been logged out successfully!!"));
     }
 
     @Test
-    @Ignore("WIP: logout behaviour changed with Spring security")
     public void shouldStayInLoginFormIfLoginUnsuccessful() throws Exception {
-        Browser browser = Application.statelessBrowser();
-        browser.open("/zombie/login/LoginForm");
-
-        WebElement usernameElement = browser.findElement(By.name("username"));
-        usernameElement.sendKeys("admin1");
-
-        WebElement passwordElement = browser.findElement(By.name("password"));
-        passwordElement.sendKeys("12Welcome1");
-
-        WebElement submitElement = browser.findElement(By.tagName("button"));
-        submitElement.click();
+        browser = BrowserSessionBuilder
+                .newStatelessSession()
+                .loggedInAs("noooo", "wrong!")
+                .build();
 
         assertThat(browser.getPageTitle(), is("Zombie Dash : Login"));
     }
