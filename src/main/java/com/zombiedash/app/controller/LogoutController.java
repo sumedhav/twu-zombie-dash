3aa88b792f89f9dad3bfd910dab4c51375beb557
tejinders <mail2_tejinder@yahoo.com>
2012-08-31 12:02:08 +0530

    tejinder, silvio - merging with server changes

--- src/main/java/com/zombiedash/app/controller/LogoutController.java
+++ src/main/java/com/zombiedash/app/controller/LogoutController.java
@@ -2,11 +2,13 @@ package com.zombiedash.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/logout")
 public class LogoutController {
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView logout() {
         ModelAndView modelAndView = new ModelAndView("loginform");
         modelAndView.addObject("messageToBeDisplayed", "Successful logout");
