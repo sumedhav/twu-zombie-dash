f9ea70987990138fe85017f3113a35cda338517a
Yahya <yahyapo@thoughtworks.com>
2012-09-03 16:27:25 +0530

    [Venki,Yahya] - Removed display of admin from list of users and refactored tests in UsersPageTest to satisfy the change

--- src/system-tests/java/com/zombiedash/app/web/page/tests/UsersPageTest.java
+++ src/system-tests/java/com/zombiedash/app/web/page/tests/UsersPageTest.java
@@ -1,15 +1,23 @@
 package com.zombiedash.app.web.page.tests;
 
+import com.zombiedash.app.repository.UserRepository;
+import com.zombiedash.app.web.Application;
+import com.zombiedash.app.web.Browser;
 import com.zombiedash.app.web.page.tests.helper.BrowserSessionBuilder;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class UsersPageTest extends BasePageTest {
+
     @Before
     public void setupSession()
     {
@@ -36,26 +44,21 @@ public class UsersPageTest extends BasePageTest {
 
     @Test
     public void shouldGoToUserDetailsPageOnClickingThatUserLink() throws Exception {
-
-        String nameOfSelectedUser = browser.findElement(By.id("username_value_1")).getText();
-        browser.findElement(By.id("username_value_1")).click();
-        assertThat(browser.getPageTitle(), is("Zombie Dash : User Details"));
-
-        assertThat(browser.findElement(By.id("name_value")).getText(), is(equalTo(nameOfSelectedUser)));
-    }
-
-    @Test
-    public void shouldDisplayErrorWhenDeletingAdmin() {
-        browser.stop();
-        browser= BrowserSessionBuilder.
-                newJavascriptEnabledSession()
+        browser= BrowserSessionBuilder
+                .newStatelessSession()
                 .loggedInAsAdmin()
                 .build()
-                .open("/app/zombie/admin/users/list");
-        browser.clickOn("username_value_1");
-        browser.clickOn("delete_user");
-        browser.alertOk();
-        assertThat(browser.getPageTitle(),is("Error Page"));
+                .open("/app/zombie/admin/user/create");
+
+        browser.inputTextOn("username","username")
+                .inputTextOn("password","password123")
+                .inputTextOn("name","yahya")
+                .inputTextOn("email","email@email.com").clickOn("submit");
+
+        String nameOfSelectedUser = browser.findElement(By.id("username_value_2")).getText();
+        browser.findElement(By.id("username_value_2")).click();
+        assertThat(browser.getPageTitle(), is("Zombie Dash : User Details"));
+        assertThat(browser.findElement(By.id("name_value")).getText(), is(equalTo(nameOfSelectedUser)));
     }
 
 }
