e607949fef584e9d9cafabad755ede961e825852
Charles <clmkimpo@thoughtworks.com>
2012-09-06 12:36:16 +0530

    #ZOM010-Attendee-Views-Score Charles+Jules: Added a UserAuthenticationSuccessHandler so we can redirect the Attendee to the dashboard

---
+++ src/main/java/com/zombiedash/app/security/UserAuthenticationSuccessHandler.java
@@ -0,0 +1,18 @@
+package com.zombiedash.app.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// Responsible for controlling the flow after a successful authentication
+public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.sendRedirect("zombie/admin/home");
+    }
+}
