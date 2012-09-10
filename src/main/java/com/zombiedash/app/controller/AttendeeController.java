414042aae1fe164658200fb884820fca34416d57
Charles <clmkimpo@thoughtworks.com>
2012-09-10 10:05:16 +0530

    [Jules, Charles] - Refactored AttendeeConference #Attendee View Score.

--- src/main/java/com/zombiedash/app/controller/AttendeeController.java
+++ src/main/java/com/zombiedash/app/controller/AttendeeController.java
@@ -1,20 +1,39 @@
 package com.zombiedash.app.controller;
 
+import com.zombiedash.app.model.Attendee;
+import com.zombiedash.app.repository.AttendeeRepository;
+import com.zombiedash.app.repository.ConferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@RequestMapping("/conference/user")
-public class AttendeeController {
-        public AttendeeController() {
-        }
+import java.util.UUID;
 
-        @RequestMapping(value="home",method = RequestMethod.GET)
-        public ModelAndView showCustomerPage() {
-            ModelAndView modelAndView = new ModelAndView("attendee");
-            return modelAndView;
-        }
+@Controller
+@RequestMapping("/zombie/attendee/{attendeeId}/home")
+public class AttendeeController {
+
+    private ConferenceRepository conferenceRepository;
+    private AttendeeRepository attendeeRepository;
+
+    @Autowired
+    public AttendeeController(ConferenceRepository conferenceRepository, AttendeeRepository attendeeRepository) {
+        this.conferenceRepository = conferenceRepository;
+        this.attendeeRepository = attendeeRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView display(@PathVariable UUID attendeeId) {
+        ModelAndView modelAndView = new ModelAndView("attendee");
+        Attendee attendee = attendeeRepository.findAttendeeById(attendeeId);
+        UUID conferenceId = attendee.getConferenceId();
+        modelAndView.addObject("attendee", attendee);
+        modelAndView.addObject("conference", conferenceRepository.findConferenceById(conferenceId));
+
+        return modelAndView;
+    }
 
 }
